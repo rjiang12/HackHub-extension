dcaf5e78b022c8313e08f57f83b7b676be7458d5
rjiang12 <roy.jiang.4757@gmail.com>
2022-06-22 16:02:36 -0700

    toggle works

--- darkModeOff.js
+++ darkModeOff.js
@@ -1,1 +1,4 @@
-alert("off!");
+(function() {
+  const html = document.querySelector("html");
+  html.style.filter = 'invert(0) hue-rotate(0deg)';
+})();
--- darkModeOn.js
+++ darkModeOn.js
@@ -1,1 +1,5 @@
-alert("on!");
+(function() {
+  const html = document.querySelector("html");
+  html.style.filter = 'invert(100%) hue-rotate(180deg)';
+})();
+

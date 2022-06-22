810c2b9a38ca50e0a049c6bd27b2cb4463288386
rjiang12 <roy.jiang.4757@gmail.com>
2022-06-22 15:48:31 -0700

    push

---
+++ popup.js
@@ -0,0 +1,19 @@
+if(document.querySelector(".popup_div") && document.querySelector(".button") && document.querySelector(".darkToggle")) {
+  const darkToggle = document.querySelector(".darkToggle");
+  let darkMode = false;
+  
+  darkToggle.addEventListener("click", function() {
+    if(!darkMode) {
+      darkMode = true;
+      chrome.tabs.executeScript({
+        file:
+      }); 
+    }
+    else {
+      darkMode = false;
+      chrome.tabs.executeScript({
+        file:
+      }); 
+    }
+  })
+}
